9059de39caa2bd460acd76591c322ba538845dce
Ryan Gavin <ryan.michael.gavin@gmail.com>
2024-12-27 18:48:19 -0500

    A decent amount of design

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "conversations#index"
+
+  # Defines the route for viewing a conversation
+  resource :conversations
+
 end
