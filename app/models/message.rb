e318322a407b3d1e41ca49a58d5690ca692b25d4
Ryan Gavin <ryan.michael.gavin@gmail.com>
2025-01-01 17:32:28 -0500

    Fix ordering

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ApplicationRecord
 
   validates :content, presence: true
   validates :role, presence: true
+
+  # Implicitly order by created_at descending to show the newest messages first
+  default_scope { order(created_at: :desc) }
 end
