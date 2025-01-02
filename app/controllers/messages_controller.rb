6cb7c5a2d4fda1ed03646590e3a5820a3176060c
Ryan Gavin <ryan.michael.gavin@gmail.com>
2025-01-01 22:27:07 -0500

    Looking really nice

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,13 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create!(message_params.merge(role: "user"))
 
+    # When a message is created from the UI, we respond with a stream if the client supports turbo.
+    # The SPA listens on websockets for new messages and appends them to the conversation.
+    # The response here is essentially a fallback in case the client does not support websockets.
+    # If the client does not support turbo, we redirect to the conversation page.
     respond_to do |format|
-      format.html { redirect_to conversation_path(@message.conversation) }
       format.turbo_stream
+      format.html { redirect_to conversation_path(@message.conversation) }
     end
   end
 
--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,16 @@ class Message < ApplicationRecord
 
   # Implicitly order by created_at descending to show the newest messages first
   default_scope { order(created_at: :desc) }
+
+  # Whenever a message is created, we want to broadcast a message to the conversation channel
+  after_create_commit { broadcast_prepend_to conversation }
+
+  # Whenever a message is created by a user, we need to trigger a response from the bot
+  after_create_commit :trigger_bot_response, if: -> { role == "user" }
+
+  # This method is called whenever a message is created by a user
+  def trigger_bot_response
+    # We want to create a new message from the bot in the same conversation
+    Message.create!(content: "I'm a bot! 🤖", role: "assistant", conversation: conversation)
+  end
 end
