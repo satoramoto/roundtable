9f48d55f6019805f272dc3d2908bc43a0c3d2cf3
Ryan Gavin <ryan.michael.gavin@gmail.com>
2025-02-05 01:38:05 -0500

    support openapi.com

--- app/clients/open_ai_client.rb
+++ app/clients/open_ai_client.rb
@@ -7,6 +7,8 @@ require "json"
 
 class OpenAiClient
   BASE_URL = ENV.fetch("OPEN_AI_BACKEND") { "http://localhost:11434" }
+  MODEL = ENV.fetch("OPEN_AI_MODEL_NAME") { "llama3.2" }
+  AUTH_TOKEN = ENV["OPEN_AI_API_KEY"]
 
   def initialize(base_url: BASE_URL)
     @connection = Faraday.new(url: base_url) do |conn|
@@ -23,13 +25,15 @@ class OpenAiClient
     #
     # The "temperature" controls the randomness of the response
     # Higher values like 0.8 will generate more creative responses, but lower values like 0.2 will be more predictable
-    payload = { model: "llama3.2", messages: messages, temperature: 0.8, stream: true }
+    payload = { model: MODEL, messages: messages, temperature: 0.8, stream: true }
 
     # The response from the API is streamed in chunks
     # We yield each chunk to the caller so they can process it
     # This is how we can update the message model in real-time
     @connection.post("/v1/chat/completions") do |req|
       req.headers["Content-Type"] = "application/json"
+      req.headers["Authorization"] = "Bearer #{AUTH_TOKEN}" if AUTH_TOKEN
+
       req.body = JSON.generate(payload)
       req.options.on_data = Proc.new do |chunk, _|
         Rails.logger.info("Received chunk: #{chunk}")
